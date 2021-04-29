9d4e9651cb9b0f86aa61d8eeef208ccb5b898c06
Cai305 <34400593+Cai305@users.noreply.github.com>
2021-04-29 20:28:09 +0200

    init

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDom from "react-dom";
+
+ReactDom.render(
+    <div>
+         <h1>This is jsx</h1>,
+    </div>,
+    
+    document.querySelector("#root")
+)
